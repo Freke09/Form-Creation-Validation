26985a2b9eef003d42d6c6d33f6b0c1b6e25d8e6
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-02 14:30:14 +0100

    Wrote script.js to validate the index.html

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()
+    })
+
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    passwordValue = password.value.trim();
+
+    let isValid = true;
+    let messages = [];
+
+    if(username.length < 3){
+        isValid = false;
+        messages.push("The username is too short");
+    }
+
+    if(!email.includes("@") && !email.includes(".")){
+        isValid = false;
+        messages.push("Invalid email address");
+    }
+
+    if(!password.length >= 8){
+        isValid = false;
+        messages.push("Password is too short");
+    }
+});
